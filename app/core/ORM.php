6b94c6ad7e2033f4841c8d782b4fa066a8ce0658
Michael Naatjes <michael.naatjes87@gmail.com>
2025-08-10 00:35:29 +0000

    feat: Add ORM class with constructor for database integration

---
+++ app/core/ORM.php
@@ -0,0 +1,18 @@
+<?php
+    /**
+     * ORM: 
+     * 
+     * @version 1.0
+     * @since 1.0:
+     *  - Created
+     */
+    class ORM {
+
+        /**
+         * Constructor
+         */
+        public function __construct(Database $db){
+            
+        }
+    }
+?>
